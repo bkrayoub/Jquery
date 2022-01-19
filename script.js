92348e92d9d83b3ac4cc8115684b85c0bf5e164f
bkrayoub <boukkour.mohamed.solicode@gmail.com>
2022-01-19 23:31:18 +0100

    add table

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-$.ajax({url: "optic boukkour.json", success: function (data){
-    console.log(data)
-}});
+let table = $(#table);
+
+table.innerHTML = "ha"
+
+
+
+
+function myAJAX() {
+    $.ajax({url: "optic boukkour.json", success: function (data){
+        console.log(data[1].name)
+    }});
+}
+myAJAX()
