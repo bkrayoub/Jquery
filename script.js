b46176fa7d73b015c273233f8773ead13991bd8d
bkrayoub <92027292+bkrayoub@users.noreply.github.com>
2022-01-20 17:00:39 +0100

    add search function

--- script.js
+++ script.js
@@ -13,19 +13,16 @@ $(document).ready(function(){
             myData += '<td>'+value.price+'</td>';
             myData += '<td>'+value.stock+'</td>';
             myData += '<td>'+'<li>'+value.provider.social+'</li>'+'<li>'+value.provider.address+'</li>'+'</td>';
-
-
-            myData += '</tr>'
+            myData += '</tr>';
         });
         $('#tbody').append(myData);
     });
 });
-//////////// Search ////////////
 
 $(document).ready(function(){
-    $("#").on("keyup", function() {
+    $("#search").on("keyup", function() {
       var value = $(this).val().toLowerCase();
-      $("#myTable tr").filter(function() {
+      $("#tbody tr").filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
       });
     });
