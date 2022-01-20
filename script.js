3f99a4c5b9bc2a089557f0e7040c1da0c9848b91
bkrayoub <92027292+bkrayoub@users.noreply.github.com>
2022-01-20 13:48:52 +0100

    add

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-let table = $(#table);
+$(document).ready(function(){
+    $.getJSON("optic boukkour.json", function(data){
+        var myData = '';
+        $.each(data, function(key, value){
+            myData += '<tr>';
+            myData += '<td>'+value.id+'</td>';
+            myData += '<td><img src="'+value.image+'" width="100px"></td>';
+            myData += '<td>'+value.name+'</td>';
+            myData += '<td>'+value.reference+'</td>';
+            myData += '<td>'+value.color+'</td>';
+            myData += '<td>'+value.brand+'</td>';
+            myData += '<td>'+value.gender+'</td>';
+            myData += '<td>'+value.price+'</td>';
+            myData += '<td>'+value.stock+'</td>';
+            myData += '<td>'+'<li>'+value.provider.social+'</li>'+'<li>'+value.provider.address+'</li>'+'</td>';
 
-table.innerHTML = "ha"
 
+            myData += '</tr>'
+        });
+        $('#tbody').append(myData);
+    });
+});
+//////////// Search ////////////
 
-
-
-function myAJAX() {
-    $.ajax({url: "optic boukkour.json", success: function (data){
-        console.log(data[1].name)
-    }});
-}
-myAJAX()
+$(document).ready(function(){
+    $("#").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
